4103478247f6b7f4addc9cbc5a7313f579277421
Ali Zafar <alizafar356@gmail.com>
2020-03-21 23:00:08 +0000

    Set up API call functions for coronavirus data

--- src/main/get_corona_data.py
+++ src/main/get_corona_data.py
@@ -1,7 +1,5 @@
 import requests
 
-'https://api.xapix.dev/coronavirus-data/covid-19-api-repository/v2/total/US'
-
 def global_timeline():
     api_response = requests.get('https://api.xapix.dev/coronavirus-data/covid-19-api-repository/v2/global_timeline')
 
@@ -13,4 +11,18 @@ def latest():
     return api_response.text
 
 def locations():
-    api_response = requests.get()
+    api_response = requests.get('https://api.xapix.dev/coronavirus-data/covid-19-api-repository/v2/locations?timelines=true&country_code=US')
+
+    return api_response.text
+
+# TODO: Invalid country codes just return empty data structures. Some checking for this should be done somewhere.
+
+def timeline(country_code):
+    api_response = requests.get('https://api.xapix.dev/coronavirus-data/covid-19-api-repository/v2/timeline/{}'.format(country_code))
+
+    return api_response.text
+
+def total(country_code):
+    api_response = requests.get('https://api.xapix.dev/coronavirus-data/covid-19-api-repository/v2/total/{}'.format(country_code))
+
+    return api_response.text
