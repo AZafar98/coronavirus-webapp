7a5fba9a4da1bd9828dc94695965c77f49f10e81
Ali Zafar <alizafar356@gmail.com>
2020-03-27 12:55:44 +0000

    Calculate case numbers and return in suitable way for Flask

--- src/main/process_corona_data.py
+++ src/main/process_corona_data.py
@@ -4,6 +4,7 @@ from pprint import pprint
 import pandas as pd
 import os
 import warnings
+import re
 
 # We need to do some stuff with the data returned by the API calls to display the relevant info
 
@@ -130,6 +131,18 @@ print(confirmed.columns)
 
 
 def data_for_country(data, country, province = None):
+    """
+    Get the data for specified country and (optional) province.
+    :param pd.DataFrame data: COVID data returned from get_corona_data
+    :param str country: Country name. Specify None to return all data.
+    :param str province: Province name
+    :return: Data for specified country and province
+    """
+
+    # Option to return data for all countries
+    if country is None:
+        country_data = data.copy()
+        return country_data.reset_index(drop=True)
 
     country_data = data.loc[data['Country/Region'].str.upper() == country.upper(),:].reset_index(drop=True)
     if len(country_data) == 0:
@@ -140,18 +153,101 @@ def data_for_country(data, country, province = None):
     country_data['Province/State'] = country_data['Province/State'].fillna('None')
 
     if province is not None:
+        provinces = country_data['Province/State'].unique().tolist()
         country_data = country_data.loc[country_data['Province/State'].str.upper() == province.upper(), :]
 
         # Invalid province specified
         if len(country_data) == 0:
-            raise ValueError("Invalid province '{}' specified for country '{}'.".format(province, country))
+            raise ValueError("Invalid province '{}' specified for country '{}'. Specify province from \n {}."
+                             .format(province, country, provinces))
 
     if len(country_data) > 1:
         provinces = country_data['Province/State'].unique().tolist()
         warnings.warn("More than one province available for {}. Please specify province from {}, or all will be used."
                       .format(country, provinces))
 
+    country_data = country_data.reset_index(drop=True)
     return country_data
 
 
-test = data_for_country(confirmed, 'United Kingdom', province='None')
+def get_latest_figures(data, period='TOTAL'):
+    """
+    Calculate relevant figures to show to user
+    :param pd.DataFrame data:
+    :param str period: Either 'Total', '24hrs' or '7days'
+    :return:
+    """
+
+    # This will assume that the rightmost column contains the most recent data.
+    # Note also that the data is aggregated, so total is just given by the rightmost column value
+    # TODO: Check date values properly to ensure data is from correct periods
+
+    # Get a list of only the date columns
+    date_pat = re.compile('\d{1,2}/\d{1,2}/\d{1,2}')
+    cols = data.columns.tolist()
+    date_cols = [col for col in cols if date_pat.match(col)]
+
+    if period.upper() == "TOTAL":
+        dates = [date_cols[-1]]
+        total_df = data.loc[:,dates]
+
+        cases_sum = total_df.sum(axis=0)
+        cases = cases_sum.iat[0]
+
+        # Also return the dates the data is from so we can display this
+        return cases, date_cols[-1]
+
+    if period.upper() == '24H':
+        # Now need to calculate the difference between the two most recent data points
+        dates = date_cols[-2:]
+        total_df = data.loc[:, dates]
+
+        # New cases in the period is just the difference between the first and last columns since the data is aggregated
+        new_cases_df = total_df.iloc[:, -1] - total_df.iloc[:, 0]
+
+        new_cases_sum = new_cases_df.sum(axis=0)
+
+        dates_used = ' - '.join([date_cols[-2], date_cols[-1]])
+
+        # Also return the dates the data is from so we can display this
+        return new_cases_sum, dates_used
+
+    if period.upper() == '7DAYS':
+        # Now need to calculate the difference between the two most recent data points
+        dates = date_cols[-7:]
+        total_df = data.loc[:, dates]
+
+        # New cases in the period is just the difference between the first and last columns since the data is aggregated
+        new_cases_df = total_df.iloc[:, -1] - total_df.iloc[:, 0]
+
+        new_cases_sum = new_cases_df.sum(axis=0)
+
+        dates_used = ' - '.join([date_cols[-7], date_cols[-1]])
+
+        # Also return the dates the data is from so we can display this
+        return new_cases_sum, dates_used
+
+
+def display_covid_cases(cases = True, period = 'Total'):
+    """
+
+    :param bool cases: If True, return number of cases in period. If False, return dates used for that period.
+    :param period: Currently either Total, 24H or 7Days
+    :return:
+    """
+
+    period = period.upper()
+
+    # For now, this is set up to use UK data only. Need to figure out Flask a bit better to pass parameters down.
+    uk_data = data_for_country(confirmed, 'United Kingdom', province='None')
+
+    if period not in ['TOTAL', '24H', '7DAYS']:
+        raise ValueError("Invalid time period. Must be either 'TOTAL', '24H', or '7DAYS'")
+
+    uk_cases, dates = get_latest_figures(uk_data, period=period)
+
+    if cases is True:
+        return uk_cases
+    else:
+        return dates
+
