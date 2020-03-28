efe0927c042271d9a74bc5b4370463d483e6f693
Ali Zafar <alizafar356@gmail.com>
2020-03-28 15:19:40 +0000

    Feed other data

--- src/flask/application.py
+++ src/flask/application.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 from src.main.process_corona_data import display_covid_cases
-from src.main.get_phe_data import get_heart_data
+from src.main.get_phe_data import get_heart_data, get_depression_data
 
 footer_text = '</body>\n</html>'
 
@@ -15,7 +15,8 @@ def index():
                            cases_24h_dates = display_covid_cases(cases=False, period='24h'),
                            cases_7days = display_covid_cases(cases=True, period='7days'),
                            cases_7days_dates = display_covid_cases(cases=False, period='7days'),
-                           heart_data = get_heart_data())
+                           heart_data = get_heart_data(),
+                           depression_data = get_depression_data())
 
 application.add_url_rule('/', 'index', index)
 
