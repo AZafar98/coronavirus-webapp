0c8a9b8320f234e42e7351cdff4e91263991408d
Ali Zafar <alizafar356@gmail.com>
2020-04-02 14:48:45 +0100

    Revert "PEP8"
    This reverts commit 65179b283d31cbfe188aaaca19988b24c7adad1b.

--- src/main/download_corona_data.py
+++ src/main/download_corona_data.py
@@ -1,13 +1,10 @@
 import pandas as pd
 from pathlib import Path
 import os
-from src.flask.settings import RUNNING_LOCALLY
+
 # This is to redeploy the webapp after the data has been downloaded.
-
-
 def update():
     os.utime('/var/www/azafar98_pythonanywhere_com_wsgi.py')
-
 
 def download_corona_data():
     """
@@ -42,11 +39,7 @@ def download_corona_data():
 
     return 0
 
+download = download_corona_data()
 
-if not RUNNING_LOCALLY:
-    download = download_corona_data()
-
-    if download == 0:
-        update()
-else:
-    download_corona_data()
+if download == 0:
+    update()
