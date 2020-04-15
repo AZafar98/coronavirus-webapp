3bc00081aa6435a14a789b84727dd4f53433663b
Ali Zafar <alizafar356@gmail.com>
2020-04-15 23:18:14 +0100

    bring back js

---
+++ src/flask/static/js/main.js
@@ -0,0 +1,13 @@
+//Initialise tooltips
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip({html:true,
+      trigger: 'hover focus',
+      delay: {"show": 500, "hide": 1000 }})
+});
+
+//TODO: Make the navbar highlight properly
+
+$('.navbar-nav .nav-link').click(function(){
+    $('.navbar-nav .nav-link').removeClass('active');
+    $(this).addClass('active');
+});
