e9fd4375ba37fcd34cc29c2abcc34423bce438f6
Ali Zafar <alizafar356@gmail.com>
2020-04-17 11:22:30 +0100

    Graph styling

--- src/flask/static/js/index.js
+++ src/flask/static/js/index.js
@@ -352,9 +352,10 @@ let covidGraph = new Dygraph(document.getElementById('covidTimeSeries'),
         labelsDiv: document.getElementById('covidLegend'),
         hideOverlayOnMouseOut: true,
         labelsSeparateLines: true,
-        xRangePad: 25,
+        rightGap: 25,
         titleHeight: 50,
-        colors: ['#a6cee3','#1f78b4','#b2df8a','#33a02c','#fb9a99','#e31a1c','#fdbf6f','#ff7f00','#cab2d6']
+        colors: ['#a6cee3','#1f78b4','#b2df8a','#33a02c','#fb9a99','#e31a1c','#fdbf6f','#ff7f00','#cab2d6'],
+        width: '100%'
 
     }
 );
