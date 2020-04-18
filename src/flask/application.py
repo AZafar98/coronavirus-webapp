a0d7796136818cc86b69d6e1deb9e0d48a54b489
Ali Zafar <alizafar356@gmail.com>
2020-04-18 01:28:54 +0100

    Add impacts page and tidy some CSS

--- src/flask/application.py
+++ src/flask/application.py
@@ -77,12 +77,17 @@ def donate():
     return render_template('donate.html')
 
 
+def impacts():
+    return render_template('impacts.html')
+
+
 application.add_url_rule('/', 'index', index)
 application.add_url_rule('/about', 'about', about)
 application.add_url_rule('/donate', 'donate', donate)
+application.add_url_rule('/impacts', 'impacts', impacts)
 
 # Run the app. This if block is only entered when running on local machine, so application.debug = True *should* be fine
-# to be left here...
+# to be left here.....
 if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
     # removed before deploying a production app.
